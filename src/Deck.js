abd487bf6e8591910124919f3337f320f9327a95
Christian Wise <djnarcissist@Christians-MacBook-Pro.local>
2023-11-10 21:45:20 -0500

    Completed game

--- src/Deck.js
+++ src/Deck.js
@@ -2,36 +2,65 @@ import React, { useState } from "react";
 import axios from 'axios'
 import Card from "./Card";
 
-
-
-
 const Deck = () => {
-    const [deckId, setDeckId] = useState(0);
+    const [deckId, setDeckId] = useState(null);
     const [remaining, setRemaining] = useState(0);
-    const [currentCardIdx, setCurrentCardIdx] = useState(0);
-    const currentCard = deckOfCards[currentCardIdx];
-    
+    const [currentCard, setCurrentCard] = useState(null);
 
-    const newDeck = () => {
+    const newDeck = async () => {
         try {
             const response = await axios.get('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1');
             const { deck_id, remaining } = response.data;
             setDeckId(deck_id);
             setRemaining(remaining);
+            setCurrentCard(null); // Fix here
         } catch (error) {
             console.error('Error creating new deck', error);
         }
     };
- 
-    const nextCard = async() => {
-        setCurrentCardIdx((prevIdx) => (prevIdx + 1) % deckOfCards.length);
+
+    const shuffleDeck = async () => {
+        try {
+            if (!deckId) {
+                alert('Error: Deck not initialized. Click "New Deck" first')
+                return;
+            }
+            const response = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/shuffle/`);
+            setRemaining(response.data.remaining);
+            setCurrentCard(null);
+         }   catch (error) {
+            console.error('Error shuffling deck', error );
+        }
+    };
+
+    const nextCard = async () => {
+        try {
+            if (!deckId) {
+                alert('Error: Deck not initialized. Click "New Deck" first.');
+                return;
+            }
+
+            if (remaining === 0) {
+                alert('Error: no cards remaining');
+                return;
+            }
+
+            const response = await axios.get(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`);
+            const { cards, remaining: updatedRemaining } = response.data;
+            setCurrentCard(cards[0]);
+            setRemaining(updatedRemaining);
+        } catch (error) {
+            console.error('Error drawing card', error);
+        }
     };
 
     return (
         <div>
-            <h2> Card Deck</h2>
-            <Card card={currentCard} />
-            <button onClick={nextCard}>Next Card</button>
+            <h2>Card Deck</h2>
+            {currentCard && <Card card={currentCard} />}
+            <button onClick={newDeck}>New Deck</button>
+            <button onClick={nextCard}>Draw Card</button>
+            <button onClick={shuffleDeck}> Shuffle Deck</button>
         </div>
     );
 };
